7130e578f2369817213f525adc649bb58af8f372
Jigau Cristian <cristi.jigau@gmail.com>
2021-05-24 17:05:10 +0300

    First commit

--- src/components/FirstComponent.js
+++ src/components/FirstComponent.js
@@ -1,31 +1,84 @@
-import React, { useState } from 'react';
+import React, { useContext, useCallback, useEffect, useRef } from 'react';
 import { Form, Button, Col } from 'react-bootstrap';
+import { TODO_CONTEXT } from '../context';
 
-const FirstComponent = ({ setTodos, todos }) => {
-  const [todo, setTodo] = useState({ id: undefined, value: '' });
-  return (
-    <Form
-      className="mb-3"
-      onSubmit={e => {
-        e.preventDefault();
-        if (todo.id) setTodos([...todos, todo]);
+const FirstComponent = ({ todo, setTodo }) => {
+  const { dispatch, action, todos } = useContext(TODO_CONTEXT);
+  const inputRef = useRef();
+
+  const focus = () => {
+    inputRef.current.focus();
+  };
+
+  useEffect(() => {
+    focus();
+  }, []);
+
+  const checkValue = value => todos.find(todo => todo.value === value);
+
+  const handleSubmit = useCallback(
+    e => {
+      e.preventDefault();
+      if (todo.id) {
+        if (action === 'add') {
+          if (!checkValue(todo.value)) {
+            dispatch({
+              type: 'ADD_TODOS',
+              payload: {
+                id: todo.id,
+                value: todo.value,
+                completed: false,
+              },
+            });
+          }
+        }
         setTodo({
           id: undefined,
           value: '',
         });
-      }}
-    >
+      }
+      if (action === 'edit') {
+        if (!checkValue(todo.value)) {
+          dispatch({
+            type: 'EDIT_TODOS',
+            payload: {
+              id: todo.id,
+              value: todo.value,
+            },
+          });
+          dispatch({
+            type: 'SET_ACTION',
+            payload: {
+              action: 'add',
+            },
+          });
+        }
+      }
+    },
+    [todo.value, todo.id, dispatch]
+  );
+
+  return (
+    <Form className="mb-3" onSubmit={handleSubmit}>
       <Form.Row className="align-items-end">
         <Col xs={12} lg={10} xl={10}>
           <Form.Control
             type="input"
+            ref={inputRef}
             value={todo.value}
-            onChange={({ target: { value } }) =>
-              setTodo({
-                id: Math.random() * 100,
-                value,
-              })
-            }
+            onChange={({ target: { value } }) => {
+              action === 'add'
+                ? setTodo({
+                    id: Math.random() * 100,
+                    value,
+                    completed: false,
+                  })
+                : setTodo({
+                    id: todo.id,
+                    value,
+                    completed: false,
+                  });
+            }}
           />
         </Col>
         <Col xs={12} lg={2} xl={2}>
--- src/components/SecondComponent.js
+++ src/components/SecondComponent.js
@@ -1,29 +1,91 @@
-import React from 'react';
+import React, { useContext, useMemo, useState, useEffect } from 'react';
 import { Button, Row, Col } from 'react-bootstrap';
+import { TODO_CONTEXT } from '../context';
+import useLocalStorage from '../useLocalStorage';
 import MainContainer from './MainContainer';
 
-const SecondComponent = ({ todos, setTodos }) => {
+const SecondComponent = ({ setTodo }) => {
+  const { todos, dispatch } = useContext(TODO_CONTEXT);
+
+  // if you check the localStorage, they are saved in there.
+  const [filteredTodos, setFilteredTodos] = useLocalStorage('todos', []);
+
+  useEffect(() => {
+    setFilteredTodos(
+      todos.sort((a, b) => {
+        if (a.completed && !b.completed) return 1;
+        if (!a.completed && !a.completed) return -1;
+        if (a.completed && b.completed) return 0;
+      })
+    );
+  }, [todos]);
+
+  //just for the purpose of using useMemo
+  const total = useMemo(() => {
+    let counter = 0;
+    todos.forEach(() => {
+      counter++;
+    });
+    return counter;
+  }, [todos]);
+
+  const editHandler = (value, id) => {
+    dispatch({ type: 'SET_ACTION', payload: { action: 'edit' } });
+    setTodo({
+      id,
+      value,
+    });
+  };
+
+  const completeHandler = id => {
+    dispatch({
+      type: 'SET_COMPLETE',
+      payload: { id },
+    });
+  };
+
   return (
     <MainContainer>
-      {todos.map(({ id, value }) => (
+      {filteredTodos.map(({ id, value, completed }) => (
         <Row className="my-1" key={id}>
-          <Col xs={6} md={10}>
-            <i className="far fa-circle mr-2"></i>
-            {value}
+          <Col xs={6} md={6}>
+            <input
+              className="mr-2"
+              type="checkbox"
+              onClick={() => completeHandler(id)}
+            />
+            <span Style={completed ? 'text-decoration: line-through;' : ''}>
+              {value}
+            </span>
           </Col>
-          <Col xs={6} md={2}>
+          <Col xs={3} md={3}>
             <Button
               variant="danger"
-              onClick={() =>
-                setTodos(todos.filter(({ id: item }) => item !== id))
-              }
+              onClick={() => {
+                dispatch({
+                  type: 'DELETE_TODOS',
+                  payload: {
+                    id,
+                  },
+                });
+              }}
               block
             >
-              x
+              delete
+            </Button>
+          </Col>
+          <Col xs={3} md={3}>
+            <Button variant="info" onClick={() => editHandler(value, id)} block>
+              edit
             </Button>
           </Col>
         </Row>
       ))}
+      <Row>
+        <Col>
+          <span>Total: {total}</span>
+        </Col>
+      </Row>
     </MainContainer>
   );
 };
